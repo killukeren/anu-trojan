2f59dbbfb9ad34f12e90e4ae62a0d6514556d257
Yurichi K1llu <killusandikami@gmail.com>
2026-02-02 09:54:05 +0800

    Update stored hashed password in gecko-login.php

--- gecko-login.php
+++ gecko-login.php
@@ -60,7 +60,7 @@ function show_login_page($message = "")
 }
 
 if (!isset($_SESSION['authenticated'])) {
-    $stored_hashed_password = '$2a$12$bDCSKD1uNM2lvVlUAWlMsuyhrzAdmKTkz/2F7024areUUF2hN6ZVO'; // Use PASSWORD_DEFAULT hash u can use this site https://phppasswordhash.com/ for generate ur password >  Gunakan hash PASSWORD_DEFAULT Anda dapat menggunakan situs ini https://phppasswordhash.com/ untuk menghasilkan kata sandi Anda
+    $stored_hashed_password = '$2y$10$V6C94Z68IdWyOBX8ShTBUOYhhCjGkFrmHwTSV9KHVfW2NWSDH0mPa'; // Use PASSWORD_DEFAULT hash u can use this site https://phppasswordhash.com/ for generate ur password >  Gunakan hash PASSWORD_DEFAULT Anda dapat menggunakan situs ini https://phppasswordhash.com/ untuk menghasilkan kata sandi Anda
 
     if (isset($_POST['pass']) && password_verify($_POST['pass'], $stored_hashed_password)) {
         $_SESSION['authenticated'] = true;
